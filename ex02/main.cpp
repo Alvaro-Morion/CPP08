9350caeeee0e81d794b69d51cb9d394b36de7ad6
Alvaro Morion de eusebio <amorion-@c2r3s4.42madrid.com>
2022-09-09 19:09:44 +0200

    final

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,13 @@
 /*   By: amorion- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 18:14:44 by amorion-          #+#    #+#             */
-/*   Updated: 2022/07/06 18:32:21 by amorion-         ###   ########.fr       */
+/*   Updated: 2022/09/09 19:08:51 by amorion-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mutantstack.hpp"
 #include<iostream>
-
+#include<list>
 int main()
 {
 	std::cout << "Given Main\n";
@@ -32,11 +32,32 @@ int main()
 	--it;
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
-	std::stack<int> s(mstack);
-	
+	{
+		std::cout << "List output\n";
+		std::list<int> mstack;
+    	mstack.push_back(5);
+    	mstack.push_back(17);
+    	std::cout << mstack.back() << std::endl;
+    	mstack.pop_back();
+    	std::cout << mstack.size() << std::endl;
+    	mstack.push_back(3);
+    	mstack.push_back(5);
+    	mstack.push_back(737);
+    	mstack.push_back(0);
+		std::list<int>::iterator it = mstack.begin();
+		std::list<int>::iterator ite = mstack.end();
+    	++it;
+    	--it;
+    	while (it != ite)
+    	{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
+
 	{
 		std::cout << "Copy\n";
 		MutantStack<int> copy(mstack);
